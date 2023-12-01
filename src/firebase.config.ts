c21d0313117a522f51370cb2db7f40c6897908c8
Bananalem0n <ukasyahnata@outlook.com>
2023-12-02 01:49:41 +0700

    add keys

---
+++ src/firebase.config.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app';
+const firebaseConfig = {
+  apiKey: 'AIzaSyA2JWAkvS8tPbXSXv0g4ugMY9D_Ralxc-s',
+  authDomain: 'tracker-64690.firebaseapp.com',
+  projectId: 'tracker-64690',
+  storageBucket: 'tracker-64690.appspot.com',
+  messagingSenderId: '137528736067',
+  appId: '1:137528736067:web:bed22060078d8e46cf57f6',
+  measurementId: 'G-9P5PELR7KT',
+};
+
+export const firebase = initializeApp(firebaseConfig);
